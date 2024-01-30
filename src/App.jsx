530a24a8d0babd5554d877dbf24ed23bfec05910
aliaydogdu105 <aliaydogdu105@gmail.com>
2024-01-30 23:59:04 +0300

    feat: add tailwindCSS and DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <div>Vocabulary App</div>
+      <div className="flex justify-center">
+        <div className="mockup-window border bg-base-300 w-1/3">
+          <div className="flex justify-center px-4 py-16 bg-base-200">
+            Vocabulary App
+          </div>
+        </div>
+      </div>
     </>
   );
 }
