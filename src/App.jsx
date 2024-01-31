3230ca0cdaf3628a9ae85f1c03e89f7afd7ab9be
aliaydogdu105 <aliaydogdu105@gmail.com>
2024-01-31 23:59:01 +0300

    feat: add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <div className="flex justify-center">
-        <div className="mockup-window border bg-base-300 w-1/3">
-          <div className="flex justify-center px-4 py-16 bg-base-200">
-            Vocabulary App
-          </div>
-        </div>
-      </div>
+      <Navbar />
     </>
   );
 }
