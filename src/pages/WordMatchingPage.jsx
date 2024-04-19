1dca19335f1a6546030fd2b79e37a5d83740aaeb
aliaydogdu105 <aliaydogdu105@gmail.com>
2024-04-19 23:58:55 +0300

    feat: add some style wordMatchingPage

--- src/pages/WordMatchingPage.jsx
+++ src/pages/WordMatchingPage.jsx
@@ -11,24 +11,26 @@ const wordPairs = [
 
 const WordMatchingPage = () => {
   return (
-    <div className=" flex flex-col justify-center items-center gap-5 mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Word Matching Game</h1>
-      <div className=" flex gap-16">
-        <div className=" flex flex-col gap-5">
+    <div className=" flex flex-col justify-center items-center gap-5 w-full mx-auto px-4 py-8">
+      <h1 className="text-2xl sm:text-3xl font-bold mb-4">
+        Word Matching Game
+      </h1>
+      <div className=" flex">
+        <div className=" flex justify-center items-center gap-5 w-1/2 flex-wrap">
           {wordPairs.map((card) => (
             <div
               key={wordPairs.id}
-              className="card w-48 h-32 grid place-content-center bg-base-200 border border-base-300 shadow-xl"
+              className="card w-full sm:w-3/4 md:w-1/2 h-32 grid place-content-center bg-base-200 border border-base-300 shadow-xl hover:bg-base-300 active:scale-95"
             >
               <div className="card-body">{card.english}</div>
             </div>
           ))}
         </div>
-        <div className=" flex flex-col gap-5 flex-wrap">
+        <div className=" flex justify-center items-center gap-5 w-1/2 flex-wrap">
           {wordPairs.map((card) => (
             <div
               key={wordPairs.id}
-              className="card w-48 h-32 grid place-content-center bg-base-200 border border-base-300 shadow-xl"
+              className="card w-full sm:w-3/4 md:w-1/2 h-32 grid place-content-center bg-base-200 border border-base-300 shadow-xl hover:bg-base-300 active:scale-95"
             >
               <div className="card-body">{card.turkish}</div>
             </div>
